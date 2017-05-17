b46b72ef4513ee5ba14a2236aa98636bd3a13f3c
Xianfeng Yuan <xianfeng.yuan@menlosecurity.com>
2017-05-17 08:20:01 -0700

    initial angularjs app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stockDogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stockDogApp
+ */
+angular.module('stockDogApp')
+   .controller('MainCtrl', function ($scope, $location, WatchlistService) {
+    // [1] Populate watchlists for dynamic nav links
+    $scope.watchlists = WatchlistService.query();
+
+    // [2] Using the $location.path() function as a $watch expression
+    $scope.$watch(function () {
+      return $location.path();
+    }, function (path) {
+      if (_.contains(path, 'watchlist')) {
+        $scope.activeView = 'watchlist';
+      } else {
+        $scope.activeView = 'dashboard';
+      }
+    });
+  });
